f4c3957e61434567a04539146ffffc0cc6f7a851
RirisuV <marcinkgm@gmail.com>
2020-07-18 16:25:27 +0200

    speed-command: Implement !speed command for calculating and displaying range of pokemon's speed stat

--- src/calculations.py
+++ src/calculations.py
@@ -1,6 +1,8 @@
 import pokeapi
 import type_weakness
 import json
+import math
+from enum import Enum
 
 class Calculations:
 
@@ -8,14 +10,16 @@ class Calculations:
         self.poke_api = poke_api
 
 
-    def calc_weakness(self, first_type, second_type):
-        """
-        Method for calculating specific pokemon weakness
+    def calc_weakness(self, pkmn: dict)->type_weakness:
+        """Method for calculating specific pokemon weakness
         based on file pokemon-type-chart.json.
 
         Method returns TypeWeakness object. 
         """
         tw = type_weakness.TypeWeakness()
+
+        first_type = pkmn['types'][0]['type']['name']
+        second_type = None if len(pkmn['types']) == 1 else pkmn['types'][1]['type']['name']
 
         with open(f'src\\resources\\pokemon-type-chart.json') as type_chart_file:
             type_chart = json.load(type_chart_file)
@@ -40,3 +44,62 @@ class Calculations:
                     setattr(tw, item['name'], getattr(tw, item['name']) * 0.5)
 
             return tw
+
+
+    def get_attack_stat_for_level(self, pkmn: dict, level: int)->dict:
+        """Get pokemon attack stat range"""
+        return self.__calc_normal_stat(pkmn['stats'][self.Stats.ATTACK.value]['base_stat'], level)
+
+
+    def get_defense_stat_for_level(self, pkmn: dict, level: int)->dict:
+        """Get pokemon defense stat range"""
+        return self.__calc_normal_stat(pkmn['stats'][self.Stats.DEFENSE.value]['base_stat'], level)
+
+
+    def get_sp_attack_stat_for_level(self, pkmn: dict, level: int)->dict:
+        """Get pokemon special attack stat range"""
+        return self.__calc_normal_stat(pkmn['stats'][self.Stats.SPECIAL_ATTACK.value]['base_stat'], level)
+
+
+    def get_sp_defense_stat_for_level(self, pkmn: dict, level: int)->dict:
+        """Get pokemon special defense stat range"""
+        return self.__calc_normal_stat(pkmn['stats'][self.Stats.SPECIAL_DEFENSE.value]['base_stat'], level)
+
+
+    def get_speed_stat_for_level(self, pkmn: dict, level: int)->dict:
+        """Get pokemon speed stat range"""
+        return self.__calc_normal_stat(pkmn['stats'][self.Stats.SPEED.value]['base_stat'], level)
+
+
+    def __calc_normal_stat(self, base_stat: int, level: int)->dict:
+        """Calc range of min and max of normal stat (Atk, Def, SpA, SpD, Spe).
+
+        EV - effort value
+        IV - individual value
+        """
+        MIN_IV, MAX_IV = 0, 31,
+        MIN_EV, MAX_EV = 0, 255,
+        MIN_NATURE, MAX_NATURE = 0.9, 1.1
+
+        min_stat = self.__normal_stat_formula(base_stat, MIN_IV, MIN_EV, MIN_NATURE, level)
+        max_stat = self.__normal_stat_formula(base_stat, MAX_IV, MAX_EV, MAX_NATURE, level)
+
+        return {
+            "min_stat": min_stat,
+            "max_stat": max_stat
+        }
+
+
+    def __normal_stat_formula(self, base_stat: int, iv: int, ev: int, nature: float, level: int)->int:
+        """Math formula of normal stat (Atk, Def, SpA, SpD, Spe)."""
+        return int(((((2*base_stat + iv + (ev/4)) * level)/100) + 5) * nature)
+
+    
+
+    class Stats(Enum):
+        HP = 0
+        ATTACK = 1
+        DEFENSE = 2
+        SPECIAL_ATTACK = 3
+        SPECIAL_DEFENSE = 4
+        SPEED = 5
