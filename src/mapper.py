2a8e831458973be84c66c5dc93e77911cf742526
RirisuV <marcinkgm@gmail.com>
2020-09-12 22:25:32 +0200

    name-mapper: Add unit tests to cover NameMapper class

--- src/mapper.py
+++ src/mapper.py
@@ -13,7 +13,7 @@ class NameMapper:
         A lot of pokemon names have different variants and forms. It helps choose basic variant.
         For example species like Mimikyu has only one form in PokeApi called Mimikyu-Disquised.
         """
-        name_spelling = name_spelling.lower().replace("","-")
+        name_spelling = name_spelling.lower().replace(" ","-")
         with open(f'./src/resources/pokemon-list.json') as pokemon_mapper_file:
             pokemon_mapper = json.load(pokemon_mapper_file)
             mapped_names = list(filter(lambda x:x["name"].startswith(name_spelling), pokemon_mapper['pokemons']))
